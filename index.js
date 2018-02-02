e6598985d1087daae584dc9c8577edfa945e2d07
Vegard <vegardhauslien@gmail.com>
2018-02-02 19:11:47 +0100

    new stuff

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var builder = require('botbuilder'); 
-var apiairecognizer = require('api-ai-recognizer');
-var connector = new builder.ConsoleConnector().listen(); 
+var restify = require('restify');
+var apiairecognizer = require('api-ai-recognizer'); 
+var request = require('request'); 
+
+var server = restify.createServer(); 
+
+server.listen(process.env.port || process.env.PORT || 3978, function () { 
+    console.log('%s listening to %s', server.name, server.url); 
+}); 
+
+var connector = new builder.ChatConnector({ 
+    appId: '52028b33-32d4-4377-b336-530c28c9c604', 
+    appPassword: 'uaucFKFKU6342hkaZY9$#%' 
+});
+
 var bot = new builder.UniversalBot(connector); 
 
 var recognizer = new apiairecognizer('c16b8d5d22b3429d87eeb727cfd8c824');
